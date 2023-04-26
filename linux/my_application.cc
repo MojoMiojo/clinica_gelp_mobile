c3c33d5d59cef2182e289ffdd1af55862ac401b6
Mozart Sousa <mjasousa@sga.pucminas.br>
2023-04-26 20:06:15 -0300

    feat: implement clean architecture (#10)
    * feat: git ignore
    
    * Update .gitignore
    
    * feat: flutter create . + architecture

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "gelp_questionnaire");
+    gtk_header_bar_set_title(header_bar, "clinica_gelp_mobile");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "gelp_questionnaire");
+    gtk_window_set_title(window, "clinica_gelp_mobile");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
